432896f54ec3767ff6a9e5a6bcbb3e99320b1fea
Tássio Rocha <tassiomarcosr@outlook.com>
2020-06-06 22:38:54 -0300

    Initial Commit

---
+++ itunes-api-objc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  itunes-api-objc
+//
+//  Created by Tassio Marcos Rocha on 06/06/20.
+//  Copyright © 2020 Tassio Marcos Rocha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
